5f21e86a2048dae7cef79aa2f14acbabf1ddeaaa
Ivan Tustanivskyi <tustanivsky@gmail.com>
2025-05-16 09:23:27 +0300

    Add custom shader

---
+++ Source/HeavyCompute/HeavyCompute.Build.cs
@@ -0,0 +1,54 @@
+using UnrealBuildTool; 
+
+public class HeavyCompute: ModuleRules 
+
+{ 
+
+	public HeavyCompute(ReadOnlyTargetRules Target) : base(Target) 
+
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PrivateIncludePaths.AddRange(new string[] 
+		{
+			"HeavyCompute/Private"
+		});
+		if (Target.bBuildEditor == true)
+		{
+			PrivateDependencyModuleNames.Add("TargetPlatform");
+		}
+		PublicDependencyModuleNames.Add("Core");
+		PublicDependencyModuleNames.Add("Engine");
+		PublicDependencyModuleNames.Add("MaterialShaderQualitySettings");
+		
+		PrivateDependencyModuleNames.AddRange(new string[]
+		{
+			"CoreUObject",
+			"Renderer",
+			"RenderCore",
+			"RHI",
+			"Projects"
+		});
+		
+		if (Target.bBuildEditor == true)
+		{
+
+			PrivateDependencyModuleNames.AddRange(
+				new string[] {
+					"UnrealEd",
+					"MaterialUtilities",
+					"SlateCore",
+					"Slate"
+				}
+			);
+
+			CircularlyReferencedDependentModules.AddRange(
+				new string[] {
+					"UnrealEd",
+					"MaterialUtilities",
+				}
+			);
+		}
+	} 
+
+}
